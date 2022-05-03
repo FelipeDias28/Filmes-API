945e039c1b5db9bb0d36507403f19c6b38d10049
Felipe Dias <adfelipealmeida@gmail.com>
2022-05-03 08:28:28 -0300

    first commit

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using Filmes_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Filmes_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        // A lista será um campo da classe
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
